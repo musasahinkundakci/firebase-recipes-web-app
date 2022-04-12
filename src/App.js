78edaa607072fef1c76a2f52d1923b372e4c2338
musasahinkundakci <musa.20@windowslive.com>
2022-04-12 23:29:39 +0300

    silenced warnings via eslint

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
+// eslint-disable-next-line no-unused-vars
 import logo from './logo.svg';
 import './App.css';
+// eslint-disable-next-line no-unused-vars
 import firebase from './FirebaseConfig';
 function App() {
   return (
